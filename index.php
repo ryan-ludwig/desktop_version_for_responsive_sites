ce7dc2cd5d770d7e949a2b2ea90e87e5a9b99664
Ryan Ludwig <ryan@serostar.com>
2012-08-21 10:04:03 -0700

    Added cookie text

--- index.php
+++ index.php
@@ -7,7 +7,14 @@
 	<body>
 		<div class="container">
 			<h1>Demo Page 1</h1>
-			<p>Resize your browser window to test if responsive or not.</p>
+			<?php
+				if ($view_option == "desktop") {
+					echo "<p>Cookie currently is set to desktop.</p>";
+				} else {
+					echo "<p>Cookie is not set or is set to responsive.</p>";
+				}
+			?>
+			<p>Resize your browser window to test.</p>
 			<p>
 				<a href="index2.php">Demo Page 2</a> to see user choice persist throughout site</a>.
 			</p>
