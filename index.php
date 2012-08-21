0e5619f7c0bf92391ed9c57e849163ecfc5a869e
Ryan Ludwig <ryan@serostar.com>
2012-08-21 09:20:25 -0700

    More descriptive text

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
 	<body>
 		<div class="container">
 			<h1>Demo Page 1</h1>
+			<p>Resize your browser window to test if responsive or not.</p>
 			<p>
-				<a href="index2.php">Demo Page 2</a> to see user choice throughout site</a>.
+				<a href="index2.php">Demo Page 2</a> to see user choice persist throughout site</a>.
 			</p>
 			<?php include_once("includes/footer_links.php"); ?>
 		</div>
